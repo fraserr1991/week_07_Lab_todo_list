01097bfb6e8c7d1e7b3b124369b7a7ec3f3f0458
Fraser <fraser.russell1991@gmail.com>
2023-01-16 16:21:01 +0000

    Lab and homework MVP done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+import {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState( [
+    {id: 1, task: "Buy shopping"}, 
+    {id: 2, task: "Clean bathroom"},
+    {id: 3, task: "Car's MOT"}
+  ]
+  )
+  const [newTask, setNewTask] = useState(""
+  )
+
+  const shoppingListItems = todos.map((todo) => {
+    return (
+    <li key={todo.id}>
+      {todo.task}
+    </li>
+    )
+  })
+
+  const handleTaskInput = (evt) => {
+    setNewTask(evt.target.value)
+  }
+
+  const addTask = evt => {
+    evt.preventDefault()
+    const newTaskObject = {id: Date.now(), task: newTask}
+    const newListOfITems = [...todos, newTaskObject]
+    setTodos(newListOfITems)
+    setNewTask("")
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <header>To do list</header>
+      <ul>
+        {shoppingListItems}
+      </ul>
+      <form onSubmit={addTask}>
+        <label htmlFor="new-task">new task</label>
+        <input id="new-task" type="text" value={newTask} onChange={handleTaskInput}></input>
+        <input type="submit" value="Save new task"></input>
+      </form>
+    </main>
   );
 }
+
+// const saveNewItem = (evt) => {
+//   evt.preventDefault()
+//   const newItemObject = {id: Date.now(), name: newItem}
+//   const newListOfItems = [...items, newItemObject]
+//   setItems(newListOfItems)
+//   setNewItem("")
+// }
 
 export default App;
