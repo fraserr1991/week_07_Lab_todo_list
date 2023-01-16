4ee755236ff54eb27ed6dd2c094e3e745d7fb42c
Fraser <fraser.russell1991@gmail.com>
2023-01-16 19:51:55 +0000

    Extension started: can take radio button input to set priority and show priority of tasks on screen

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import './App.css';
 function App() {
 
   const [todos, setTodos] = useState( [
-    {id: 1, task: "Buy shopping"}, 
-    {id: 2, task: "Clean bathroom"},
-    {id: 3, task: "Car's MOT"}
+    {id: 1, task: "Buy shopping", priority: "high"}, 
+    {id: 2, task: "Clean bathroom", priority: "high"},
+    {id: 3, task: "Car's MOT", priority: "medium"}
   ]
   )
   const [newTask, setNewTask] = useState(""
   )
+  
+  const [priority, setPriority] = useState("")
 
   const shoppingListItems = todos.map((todo) => {
     return (
     <li key={todo.id}>
-      {todo.task}
+      {todo.task}<br></br>
+      {todo.priority}
     </li>
     )
   })
@@ -25,15 +28,24 @@ function App() {
     setNewTask(evt.target.value)
   }
 
+  const handlePriorityLevel = (evt) => {
+    setPriority(evt.target.value)
+  }
+
+  // const setHighPriorityBold = (todo) => {
+  //   if (todo.priority === "high")
+  //     return 
+  //   }
+
+
+
   const addTask = evt => {
     evt.preventDefault()
-    const newTaskObject = {id: Date.now(), task: newTask}
+    const newTaskObject = {id: Date.now(), task: newTask, priority: priority}
     const newListOfITems = [...todos, newTaskObject]
     setTodos(newListOfITems)
     setNewTask("")
   }
-
-  
 
   return (
     <main>
@@ -44,6 +56,16 @@ function App() {
       <form onSubmit={addTask}>
         <label htmlFor="new-task">new task</label>
         <input id="new-task" type="text" value={newTask} onChange={handleTaskInput}></input>
+        <br></br>
+        <div>
+        <ledgend>Priority</ledgend><br></br>
+        <input type="radio" id="low_priority" name="priorityLevel" value="low" onChange={handlePriorityLevel}></input>
+        <label htmlFor="low_priority">Low</label>
+        <input type="radio" id="medium_priority" name="priorityLevel" value="medium" onChange={handlePriorityLevel}></input>
+        <label htmlFor="medium_priority">Medium</label>
+        <input type="radio" id="high_priority" name="priorityLevel" value="high" onChange={handlePriorityLevel}></input>
+        <label htmlFor="high_priority">High</label>
+        </div>
         <input type="submit" value="Save new task"></input>
       </form>
     </main>
